5728c888766c63a8aaa214a37dd6719ed7ec8758
Yobuligo <hoffmannpeter82@gmail.com>
2020-03-15 14:39:46 +0100

    Initial Commit

---
+++ src/main/java/com/vpsd/DemoOAuth2Client/DemoOAuth2ClientApplication.java
@@ -0,0 +1,13 @@
+package com.vpsd.DemoOAuth2Client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOAuth2ClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoOAuth2ClientApplication.class, args);
+	}
+
+}
